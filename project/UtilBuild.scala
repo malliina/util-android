b073da6d5ab3d98d3cc86a6a6168380695f7fc80
malliina <malliina123@gmail.com>
2014-01-26 17:07:39 +0100

    added sbt-sonatype sbt plugin

--- project/UtilBuild.scala
+++ project/UtilBuild.scala
@@ -1,11 +1,13 @@
 import sbt._
 import sbt.Keys._
 import android.Keys._
+import xerial.sbt.Sonatype
+import xerial.sbt.Sonatype.SonatypeKeys
 
 object UtilBuild extends Build {
   lazy val utilProject = Project("util-android", file(".")).settings(utilSettings: _*)
   lazy val utilSettings = android.Plugin.androidBuild ++ publishSettings ++ Seq(
-    version := "0.1.1",
+    version := "0.1.2",
     platformTarget in Android := "android-18",
     libraryProject := true,
     libraryDependencies ++= Seq(
@@ -15,10 +17,12 @@ object UtilBuild extends Build {
       "org.java-websocket" % "Java-WebSocket" % "1.3.0",
       "org.scalatest" %% "scalatest" % "1.9.2" % "test"
     ),
-    (publishArtifact in packageBin in Compile) := true
+    // android-sdk-plugin sets these to false, but we want to create jars for maven
+    (publishArtifact in packageBin in Compile) := true,
+    (publishArtifact in packageSrc in Compile) := true
   )
 
-  def publishSettings = Seq(
+  def publishSettings = Sonatype.sonatypeSettings ++ Seq(
     organization := "com.github.malliina",
     publishTo <<= version(v => {
       val repo =
--- project/project/BuildBuild.scala
+++ project/project/BuildBuild.scala
@@ -13,6 +13,7 @@ object BuildBuild extends Build {
     "com.hanhuy.sbt" % "sbt-idea" % "1.6.0",
     "com.timushev.sbt" % "sbt-updates" % "0.1.2",
     "com.eed3si9n" % "sbt-buildinfo" % "0.3.0",
-    "com.typesafe.sbt" % "sbt-pgp" % "0.8"
+    "com.typesafe.sbt" % "sbt-pgp" % "0.8.1",
+    "org.xerial.sbt" % "sbt-sonatype" % "0.1.4"
   ) map addSbtPlugin
 }
